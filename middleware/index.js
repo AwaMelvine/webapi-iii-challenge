c0f25c681da76254aaf222c7ba5968e885e2b43e
Awa Melvine <melvineawa@gmail.com>
2019-07-10 12:40:58 +0100

    Scaffold validate user id middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,6 +1,20 @@
-const logger = (req, res, next) => {
-  req.timestamp = Date.now();
-  console.log({ method: req.method, url: req.url, timestamp: req.timestamp });
-};
+module.exports = {
+  logger(req, res, next) {
+    req.timestamp = Date.now();
+    console.log({ method: req.method, url: req.url, timestamp: req.timestamp });
+  },
 
-module.exports = { logger };
+  validateUserId(req, res, next) {
+    const id = Number.parseInt(req.params.id, 10);
+    if (Number.isInteger(id)) {
+      const user = {}; // find user by id from db
+      if (user) {
+        // save user as req.user
+        // next();
+      } else {
+        res.status(400).json({ message: "invalid user id" });
+      }
+    } else {
+    }
+  }
+};
