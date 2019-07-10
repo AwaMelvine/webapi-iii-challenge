0cb522676959c216941cd621195ddf97c8c83cc8
Awa Melvine <melvineawa@gmail.com>
2019-07-10 12:44:00 +0100

    Implement validate user middleware

--- middleware/index.js
+++ middleware/index.js
@@ -16,5 +16,13 @@ module.exports = {
       }
     } else {
     }
+  },
+  validateUser(req, res, next) {
+    if (!req.body) {
+      return res.status(400).json({ message: "missing user data" });
+    }
+    if (!req.body.name) {
+      return res.status(400).json({ message: "missing required name field" });
+    }
   }
 };
